f961ed0400bc63e6e5f7b3c078c112a6f8d29c1b
HaikoL <elarashl@yahoo.de>
2016-12-01 19:58:36 +0100

    basic raw distribution base

---
+++ ext/001_distribution/ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'example',
+    'description' => '',
+    'category' => 'distribution',
+    'author' => 'Haiko',
+    'author_email' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-8.9.99',
+            'version' => '',
+            'recycler' => '',
+            'filemetadata' => '',
+            'realurl' => '',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
